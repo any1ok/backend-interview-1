806d5cd390db41dc0456ef7d2451589ba1428402
any1ok <any3ok@naver.com>
2021-05-02 11:00:08 +0900

    정리2

--- src/dbms/postgre.ts
+++ src/dbms/postgre.ts
@@ -4,32 +4,27 @@ import { Sequelize } from "sequelize-typescript";
 
 import { Product } from "../models/model.product";
 import { Heart } from "../models/model.heart";
-//import { ProductReview } from "../models/product_review";
-//import { RefreshToken } from "../models/refresh_token";
+
 import { User } from "../models/model.user";
 
 const path = require("path");
 const sequelize = new Sequelize("postgres://postgres:7565@localhost:5432/postgres");
-//sequelize.authenticate();
 
-sequelize.addModels([User, Product, Heart]); //, RefreshToken, Product, ProductDibs, ProductReview]);
+
+sequelize.addModels([User, Product, Heart]);
 const mybatisMapper = require("mybatis-mapper");
 const sqlPath = path.join(__dirname, "..", "..", ".", '/src/sql');
 console.log(sqlPath);
 mybatisMapper.createMapper([`${sqlPath}/product.xml`]);
-//sequelize.sync({ force: false, });
 
-// if (process.env.MODE == "dev") {
-//     sequelize.sync({ force: true });
-// }
 
 const db = {
     User,
     Heart,
     Product,
-    sequelize, // connection instance (RAW queries)
+    sequelize,
     connection: sequelize,
-    Sequelize, // library
+    Sequelize,
     mybatisMapper
 };
 
